17a683b76e40a0e0c3a41f739b58e6d00408a4f6
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-26 00:26:40 +0200

    ProductService

--- FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Service/ProductService.java
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Service/ProductService.java
@@ -2,21 +2,27 @@ package pl.coderslab.finalproject.Shop.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.coderslab.finalproject.Shop.DTO.ProductDTO;
 import pl.coderslab.finalproject.Shop.Product;
 import pl.coderslab.finalproject.Shop.Repository.ProductRepository;
+import pl.coderslab.finalproject.Shop.Validator;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
-
+    private final Validator Validator;
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, pl.coderslab.finalproject.Shop.Validator validator) {
         this.productRepository = productRepository;
+        Validator = validator;
     }
 
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
 
-    // Other service methods for product management
+    public void createProduct(ProductDTO productDTO) {
+        Validator.validator.validate(productDTO);
+
+    }
 }
