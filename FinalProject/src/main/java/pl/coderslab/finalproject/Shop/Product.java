6346967c10ca87e9bfb18b444ff6928ac7642a3c
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-26 00:26:08 +0200

    Product

--- FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Product.java
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Product.java
@@ -1,19 +1,55 @@
 package pl.coderslab.finalproject.Shop;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is required")
     private String name;
-    private double price;
 
-    // Getters and setters
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @NotNull(message = "Category is required")
+    private Category category;
+
+
+    public Product() {
+    }
+
+    public Product(String name, Category category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
