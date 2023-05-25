cc44b4f8b95d7e0e61e332e5b6e90254eac7f807
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-26 00:25:53 +0200

    OrderItem

--- FinalProject/src/main/java/pl/coderslab/finalproject/Shop/OrderItem.java
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/OrderItem.java
@@ -1,20 +1,24 @@
 package pl.coderslab.finalproject.Shop;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
+@Table(name = "order_items")
+@Data
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private int quantity;
+
     @ManyToOne
+    @JoinColumn(name = "order_id")
     private Order order;
 
     @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
-
-    private int quantity;
-
-    // Getters and setters
 }
