3abdd33c945219d6d2c1f95c3ef5e694981ebcc1
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-26 01:09:48 +0200

    ProductService

--- FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Service/ProductService.java
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Service/ProductService.java
@@ -3,9 +3,11 @@ package pl.coderslab.finalproject.Shop.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.coderslab.finalproject.Shop.DTO.ProductDTO;
-import pl.coderslab.finalproject.Shop.Product;
+import pl.coderslab.finalproject.Shop.Entity.Product;
 import pl.coderslab.finalproject.Shop.Repository.ProductRepository;
 import pl.coderslab.finalproject.Shop.Validator;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -24,5 +26,29 @@ public class ProductService {
     public void createProduct(ProductDTO productDTO) {
         Validator.validator.validate(productDTO);
 
+    }
+
+    public Product updateProduct(Long productId, ProductDTO productDTO) {
+        Product existingProduct = productRepository.findById(productId).orElse(null);
+
+        if (existingProduct != null) {
+            // Aktualizacja pól produktu na podstawie danych z ProductDTO
+            // ...
+
+            return productRepository.save(existingProduct); // Wykonanie operacji UPDATE
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteProduct(Long productId) {
+        productRepository.deleteById(productId); // Wykonanie operacji DELETE
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        // Implementacja wyszukiwania produktów na podstawie keyword
+        // ...
+
+        return matchingProducts;
     }
 }
