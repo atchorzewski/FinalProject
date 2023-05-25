9684934780cf83bf017c2ffea733ff02810f19f3
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-25 22:55:30 +0200

    Customer

---
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Customer.java
@@ -0,0 +1,18 @@
+package pl.coderslab.finalproject.Shop;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Getters and setters
+}
