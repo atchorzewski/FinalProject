da8a0ac3d4e240e5c52c07567cd0613925e9eb8c
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-25 22:52:53 +0200

    main application

---
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/MainApplication.java
@@ -0,0 +1,11 @@
+package pl.coderslab.finalproject.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
