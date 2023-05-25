95f6d78bce358bd0ff6ba1e30361401a669ce360
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-25 23:00:18 +0200

    ProductService

--- FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Service/ProductService.java
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Service/ProductService.java
@@ -2,6 +2,8 @@ package pl.coderslab.finalproject.Shop.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.coderslab.finalproject.Shop.Product;
+import pl.coderslab.finalproject.Shop.Repository.ProductRepository;
 
 @Service
 public class ProductService {
