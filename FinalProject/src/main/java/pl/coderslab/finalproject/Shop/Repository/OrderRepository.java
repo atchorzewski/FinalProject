df9f228e5a73fe3aa28b5f8f93f6948b7cd52c7a
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-26 01:10:34 +0200

    OrderRepository

--- FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Repository/OrderRepository.java
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package pl.coderslab.finalproject.Shop.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.coderslab.finalproject.Shop.Order;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.finalproject.Shop.Entity.Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    boolean existsByOrderNumber(String orderNumber);
 }
