b53bbaa2004d1064fb2ee3220af9cdd2b5cf6dc2
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-25 22:54:05 +0200

    Product

---
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Product.java
@@ -0,0 +1,19 @@
+package pl.coderslab.finalproject.Shop;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+
+    // Getters and setters
+}
