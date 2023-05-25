be8a5c8b5bc5e3313be8360372a74ed7038f063c
Arkadiusz Tchorzewski <tchorzewski.arkadiusz@gmail.com>
2023-05-26 01:09:59 +0200

    ProductRepository

--- FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Repository/ProductRepository.java
+++ FinalProject/src/main/java/pl/coderslab/finalproject/Shop/Repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package pl.coderslab.finalproject.Shop.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.coderslab.finalproject.Shop.Product;
+import pl.coderslab.finalproject.Shop.Entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
